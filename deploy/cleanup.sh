20cefa3db616e987895c7913f2e998dc47d081a4
jmccormick2001 <jeff.mccormick@crunchydata.com>
2018-10-15 12:02:34 -0400

    change deployment resources to use OSB env var prefixes, update docs

--- deploy/cleanup.sh
+++ deploy/cleanup.sh
@@ -4,32 +4,32 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICEOSB_NAMESPACEE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIOOSB_NAMESPACE OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-if [ "$CO_CMD" = "kubectl" ]; then
-	NS="--namespace=$CO_NAMESPACE"
-fi
+#if [ "$OSB_CMD" = "kubectl" ]; then
+#	OSB_NAMESPACE="--namespace=$OSB_NAMESPACE"
+#fi
 
-$CO_CMD $NS delete secret pgo-osb pgo-osb-apiserver-secret pgo-osb-tls-secret
-$CO_CMD $NS delete serviceaccount pgo-osb
+$OSB_CMD --namespace=$OSB_NAMESPACE delete secret pgo-osb pgo-osb-apiserver-secret pgo-osb-tls-secret
+$OSB_CMD --namespace=$OSB_NAMESPACE delete serviceaccount pgo-osb
 
-$CO_CMD $NS delete clusterrolebinding pgo-osb pgo-osb-client
-$CO_CMD $NS delete clusterrole pgo-osb access-pgo-osb
+$OSB_CMD --namespace=$OSB_NAMESPACE delete clusterrolebinding pgo-osb pgo-osb-client
+$OSB_CMD --namespace=$OSB_NAMESPACE delete clusterrole pgo-osb access-pgo-osb
 
-$CO_CMD $NS delete service pgo-osb
+$OSB_CMD --namespace=$OSB_NAMESPACE delete service pgo-osb
 
-$CO_CMD $NS delete deployment pgo-osb
+$OSB_CMD --namespace=$OSB_NAMESPACE delete deployment pgo-osb
 
-$CO_CMD $NS delete clusterservicebroker pgo-osb
+$OSB_CMD --namespace=$OSB_NAMESPACE delete clusterservicebroker pgo-osb
 
 sleep 5
 
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,11 +4,11 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICEOSB_NAMESPACEE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIOOSB_NAMESPACE OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
@@ -17,31 +17,31 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DIR/cleanup.sh
 
-if [ "$CO_CMD" = "kubectl" ]; then
-	NS="--namespace=$CO_NAMESPACE"
-fi
+#if [ "$OSB_CMD" = "kubectl" ]; then
+#	OSB_NAMESPACE="--namespace=$OSB_NAMESPACE"
+#fi
 
 
-$CO_CMD $NS create secret tls pgo-osb-tls-secret --cert=$DIR/server.crt --key=$DIR/server.key -o yaml
+$OSB_CMD --namespace=$OSB_NAMESPACE create secret tls pgo-osb-tls-secret --cert=$DIR/server.crt --key=$DIR/server.key -o yaml
 
-$CO_CMD $NS create secret generic pgo-osb-apiserver-secret \
+$OSB_CMD --namespace=$OSB_NAMESPACE create secret generic pgo-osb-apiserver-secret \
         --from-file=ca=$DIR/server.crt \
         --from-file=clientcert=$DIR/server.crt \
         --from-file=clientkey=$DIR/server.key
 
-$CO_CMD create -f  $DIR/service-account.yaml
-$CO_CMD create -f  $DIR/secret.yaml
+$OSB_CMD --namespace=$OSB_NAMESPACE create -f  $DIR/service-account.yaml
+$OSB_CMD --namespace=$OSB_NAMESPACE create -f  $DIR/secret.yaml
 
-$CO_CMD create -f $DIR/cluster-role-1.yaml
-$CO_CMD create -f $DIR/cluster-role.yaml
-expenv -f $DIR/cluster-role-binding-1.yaml | $CO_CMD  create -f -
-expenv -f $DIR/cluster-role-binding.yaml | $CO_CMD  create -f -
-$CO_CMD  create -f $DIR/service.yaml
+$OSB_CMD --namespace=$OSB_NAMESPACE create -f $DIR/cluster-role-1.yaml
+$OSB_CMD --namespace=$OSB_NAMESPACE create -f $DIR/cluster-role.yaml
+expenv -f $DIR/cluster-role-binding-1.yaml | $OSB_CMD  --namespace=$OSB_NAMESPACE create -f -
+expenv -f $DIR/cluster-role-binding.yaml | $OSB_CMD  --namespace=$OSB_NAMESPACE create -f -
+$OSB_CMD  --namespace=$OSB_NAMESPACE create -f $DIR/service.yaml
 
-expenv -f $DIR/deployment.yaml | $CO_CMD  create -f -
+expenv -f $DIR/deployment.yaml | $OSB_CMD  --namespace=$OSB_NAMESPACE create -f -
 
 echo "sleeping before we create clusterservicebroker"
 sleep 15
 
-expenv -f $DIR/cluster-service-broker-no-auth.yaml | $CO_CMD  create -f -
+expenv -f $DIR/cluster-service-broker-no-auth.yaml | $OSB_CMD  --namespace=$OSB_NAMESPACE create -f -
 
